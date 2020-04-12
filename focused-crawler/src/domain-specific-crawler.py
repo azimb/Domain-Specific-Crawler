c8eb2abaf0db8351fc04bff423ea582bba5d0aa6
Azim Baghadiya <azim.baghadiya@mitel.com>
2020-04-12 14:00:40 -0400

    removed print stm, improved speed

--- focused-crawler/src/domain-specific-crawler.py
+++ focused-crawler/src/domain-specific-crawler.py
@@ -1,67 +1,87 @@
-import heapq
-import urllib.request
-import requests
-
-import nltk   
-
-import math
-import numpy as np
-
 from bs4 import BeautifulSoup
-
+import urllib.request
 from vocab import *
-from gensim.models import Word2Vec, KeyedVectors
+import numpy as np
+import re
+from unidecode import unidecode
+import requests
+import heapq
+import nltk 
+import math
 
 
 '''
+Normaloze terms
+'''
 
-# function from the paper (wtu)
-def get_term_weight(unit, doc_vocabulary, ftu):
-	weight = 0.0 # this will be the output
-	 
-	 N = ? # number of feature collections
-	 nt = ? # number of units where term occurs
-	 
+CLEAN_CHARS = ['+', '=', '^', '*', '~', '#', '_', '\\']
+CLEAN_CHARS += ['(', ')', '[', ']', '{', '}', '<', '>']
+CLEAN_CHARS += ['\'', '"', '`', '%']
+clean_rx = '[' + re.escape(''.join(CLEAN_CHARS)) + ']'
+def clean(s):
+	'''
+	Clean string by removing unwanted characters.
+	'''
+	s = re.sub(clean_rx, u'', s)
+	s = u' '.join(s.split())
+	return s
+
+NORM_CHARS = ['.', ',', ':', '?', '!', ';', '-', '/', '|', '&']
+norm_rx = '[' + re.escape(''.join(NORM_CHARS)) + ']'
+def normalize(s):
+	lemmatizer = WordNetLemmatizer()
+	s = lemmatizer.lemmatize(s)
 	
-	num = ftu * math.log(N/nt)
-	denom = 0.0
-	for (term, freq) in doc_vocabulary.items():
-		fru = ? # number of times term occurs in the unit/para
-		denom += ((fru* math.log(N/nt))**2)
-	
-	denom = math.sqrt(denom)
-	
-	return num/denom
-	
+	'''
+	Normalize string by removing punctuation, capitalization, diacritics.
+	'''
+	# Replace diactritics
+	s = unidecode(s)
+	# Remove unwanted characters
+	s = clean(s)
+	# Remove capitalization
+	s = s.lower()
+	# Replace regular punctuation by spaces
+	s = re.sub(norm_rx, u' ', s)
+	s = u' '.join(s.split())
+	# Remove double consonants
+	if len(s) >= 2 and s[-1] == s[-2]:
+		s = s[:-1]
+	return s
 
 
-# input: para as a string
-def make_unit_vector(unit, doc_vocabulary):
-	
-	
-	ftu = ? # freq of term in the unit
-	
-	
-	get_term_weight(unit, doc_vocabulary, ftu)
 
-'''	
-
-def getTermFrequency(block, term):
+def getTermFrequency(block, term, wordList):
 	count = 0
-	wordList = block.get_text().split()
+	#wordList = block.get_text().split()
 	for word in wordList:
+	
+		# normalize
+		word = normalize(word)
+	
 		if(word == term):
 			count += 1
 	
 	return count
 	
-def make_unit_vector(block_list, block, doc_vocabulary):
+def make_unit_vector(block_list, block, doc_vocabulary):	
 	wordList = block.get_text().split()
 	unitVector = {}
+	
+	lemmatizer = WordNetLemmatizer()
+	
 	for word in wordList:
 		'''Calculate word weight in this contentblock'''
-		print('Calculating unit vector for word = {}'.format(word))
-		ftu = getTermFrequency(block, word)
+		#print('Calculating unit vector for word = {}'.format(word))
+		'''
+		word = unidecode.unidecode(word)
+		word = word.lower()
+		word = lemmatizer.lemmatize(word)
+		'''
+		
+		word = normalize(word)
+		
+		ftu = getTermFrequency(block, word, wordList)
 		nt = determineWordBlockFrequency(block_list, word)
 		N = len(block_list)
 		M = sum(doc_vocabulary.values())
@@ -69,13 +89,13 @@ def make_unit_vector(block_list, block, doc_vocabulary):
 		denominator = getDenom(doc_vocabulary, N, nt)
 		unitVector[word] = numerator/denominator
 		
-	print('Unit vector = {}'.format(unitVector))
+	#print('Unit vector = {}'.format(unitVector))
 	return unitVector 
 
 
 def getNumerator(ftu, N, nt):
 	numerator = (ftu * math.log10(N/nt))
-	print('Numerator = {}'.format(numerator))
+	#print('Numerator = {}'.format(numerator))
 	return numerator
 	
 def getDenom(doc_vocabulary, N, nt):
@@ -86,7 +106,7 @@ def getDenom(doc_vocabulary, N, nt):
 		denom += math.pow((fru * math.log10(N/nt)), 2)
 		
 	denom = math.sqrt(denom)
-	print('Denominator = {}'.format(denom))
+	#print('Denominator = {}'.format(denom))
 	return denom
 	
 def determineWordBlockFrequency(block_list, term):
@@ -101,6 +121,10 @@ def determineWordBlockFrequency(block_list, term):
 def checkBlockForWord(block, term):
 	wordList = block.get_text().split()
 	for word in wordList:
+		
+		# normalize
+		word = normalize(word)
+		
 		if(word == term):
 			return True
 			
@@ -144,11 +168,7 @@ def download_html(url):
 	return html
 
 	
-def getTopicVector(vocabulary):
-	model = Word2Vec(vocabulary, min_count=1, size=len(vocabulary))
-	print("Done setting up Word2Vec model...")
-	model['pokemon']
-	return "Done"
+	
 '''
 Function: to perfor the core crawling
 Parameters:
@@ -157,27 +177,30 @@ Parameters:
 '''
 def crawl(url_queue):
 
-
 	# Step 1:  Dequeues the next URL
 	pair = heapq.heappop(url_queue)
 	priority, url = pair[0], pair[1]
 	
 	# Step 2: Fetch the HTML of the url
 	html = download_html(url)
-	
-	
-	
-	vocabulary = get_vocab(html)
 
+	vocabulary = get_vocab(html)
 	
-	print("paragraphs")
+	topic_vector = ?
+	threshold = ?
+	
+	url_queue = lpe(url_queue, html, topic_vector, vocabulary, threshold)
+	
+	
+	
+	#print("paragraphs")
 	block_list = retrieve_content_blocks(html)
 	unitVectorList = []
 	for index, b in enumerate(block_list):
-		print(b.get_text())
+		#print(b.get_text())
 		'''print(b.get_text().split())'''
 		#print(b.getText())
-		print("-----------")
+		#print("-----------")
 		unitVectorList.append(make_unit_vector(block_list, b, vocabulary))
 		'''unitVectorList should have the same exact size as block_list and contain the unit vector for each block so unitVectorList[3] will be the unit vector for block_list[3]'''
 	print(unitVectorList)
@@ -187,15 +210,9 @@ def crawl(url_queue):
 	topic_vector = ?
 	threshold = ?
 	
-	
 	url_queue = lpe(url_queue, html, topic_vector, vocabulary, threshold)
 	crawl(url_queue)
 	'''
-	
-	
-
-
-
 
 
 def main():
